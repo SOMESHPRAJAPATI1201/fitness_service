4602ff409718d871b4e930c37f37f7ed33fe0e69
Somesh-Prajapati1201 <someshprajapati1201@gmail.com>
2025-12-22 23:44:58 +0530

    add: test in all service

--- userservices/src/test/java/com/fitness/userservices/UserservicesApplicationTests.java
+++ userservices/src/test/java/com/fitness/userservices/UserservicesApplicationTests.java
@@ -3,11 +3,18 @@ package com.fitness.userservices;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.fitness.userservices.UserservicesApplication.main;
+
 @SpringBootTest
 class UserservicesApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void mainMethodRuns() {
+        UserservicesApplication.main(new String[]{});
     }
 
 }
